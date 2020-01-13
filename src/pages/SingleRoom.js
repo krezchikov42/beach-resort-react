41acd4d36f330d53db81f0e01c42693e7ec40a98
Konstantin Rezchikov <rezchikov.k@husky.neu.edu>
2020-01-13 11:00:19 +0100

    adding initial template for pages

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class SingleRoom extends Component {
+    static propTypes = {
+
+    }
+
+    render() {
+        return (
+            <div>
+                SingleRoom
+            </div>
+        )
+    }
+}
+
+export default SingleRoom
