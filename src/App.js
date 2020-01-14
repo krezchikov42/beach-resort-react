54c3082719bfab5b649d79f6b1bb9f79d92ef66f
Konstantin Rezchikov <rezchikov.k@husky.neu.edu>
2020-01-14 10:38:36 +0100

    adding basic router links to pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Link}  from 'react-router-dom'
+import Home from './pages/Home'
+import Error from './pages/Error'
+import Rooms from './pages/Rooms'
+import SingleRoom from './pages/SingleRoom'
 
 function App() {
   return (
-  null
+  <Router>
+    <Route exact path='/' component={Home} />
+    <Route exact path='/rooms/' component={Rooms} />
+    <Route exact path='/rooms/:rid' component={SingleRoom} />
+  </Router>
   );
 }
-
+ 
 export default App;
