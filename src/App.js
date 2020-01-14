d9b7fc6180116a04f34d7d90be50ff73f225d258
Konstantin Rezchikov <rezchikov.k@husky.neu.edu>
2020-01-14 11:16:14 +0100

    adding default link page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route, Link}  from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, S, Switch } from 'react-router-dom'
 import Home from './pages/Home'
 import Error from './pages/Error'
 import Rooms from './pages/Rooms'
@@ -8,12 +8,15 @@ import SingleRoom from './pages/SingleRoom'
 
 function App() {
   return (
-  <Router>
-    <Route exact path='/' component={Home} />
-    <Route exact path='/rooms/' component={Rooms} />
-    <Route exact path='/rooms/:rid' component={SingleRoom} />
-  </Router>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/rooms/' component={Rooms} />
+        <Route exact path='/rooms/:rid' component={SingleRoom} />
+        <Route component={Error}></Route>
+      </Switch>
+    </Router>
   );
 }
- 
+
 export default App;
