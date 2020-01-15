8f815104bc5a625d7df0b6fbbce6f18155c2b761
Konstantin Rezchikov <rezchikov.k@husky.neu.edu>
2020-01-15 15:19:24 +0100

    creating general banner and ading to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
-        <Hero></Hero>
+        <Hero>
+            <Banner title="Luxurious Rooms" subtitle="Deluxe starting at $299">
+            <Link to="/rooms/" className="btn-primary">
+               Our Rooms
+            </Link>
+            </Banner>
+        </Hero>
     )
 }
 
