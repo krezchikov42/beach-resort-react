7c77fdb82a0b1a1f85cc8037b5c1d82b0928ff23
Konstantin Rezchikov <rezchikov.k@husky.neu.edu>
2020-01-15 13:55:24 +0100

    creating NavBar

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Home from './pages/Home'
 import Error from './pages/Error'
 import Rooms from './pages/Rooms'
 import SingleRoom from './pages/SingleRoom'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar></Navbar>
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/rooms/' component={Rooms} />
