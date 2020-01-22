2fa89a3061d021f9b38c114346415d8583599475
Konstantin Rezchikov <rezchikov.k@husky.neu.edu>
2020-01-22 16:18:05 +0100

    create room container

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -1,17 +1,23 @@
-import React from 'react'
-import { RoomConsumer } from '../context'
+import { RoomContext } from "../context"
 import Loading from './Loading'
 import RoomFilter from './RoomFilter'
 import RoomList from './RoomList'
+import React, { Component } from 'react'
 
-export default function RoomsContainer() {
-    return (
-        <RoomConsumer>{value => {
-            const {loading,sortedRooms,rooms} = value
-            return <div>
-                <RoomFilter rooms/>
-                <RoomList rooms={sortedRooms}/>
-            </div>
-        }}</RoomConsumer>
-    )
+export default class RoomsContainer extends Component {
+
+    static contextType = RoomContext
+
+    render() {
+        const { loading, sortedRooms, rooms } = this.context
+
+        if (loading) {
+            return <Loading />
+        }
+
+        return <>
+            <RoomFilter rooms />
+            <RoomList rooms={sortedRooms} />
+        </div>
+    }
 }
